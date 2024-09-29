8138ffef46f28be8f894bac1f4e613dc71c1a3f3
JotiRoy01 <jotiroygit@gmail.com>
2024-09-29 10:37:46 +0600

    update according to my project

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "REPO_NAME"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "DVC-NLP-SIMPLE-USECASE"
+AUTHOR_USER_NAME = "JotiRoy01"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
 
@@ -17,10 +17,10 @@ setup(
     description="A small package for DVC",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    url=f"https://github.com/{JotiRoy01}/{DVC-NLP-SIMPLE-USECASE}",
+    author_email="jotiroygit@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
-    python_requires=">=3.6",
+    python_requires=">=3.10",
     install_requires=LIST_OF_REQUIREMENTS
 )
